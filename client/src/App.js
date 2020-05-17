6f21bd1021c75981fbb91436b8a58ee1e62be72c
Madison Kanna <madison@mediavine.com>
2020-05-16 18:29:08 -0700

    basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [url, setUrl] = useState('')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter a link.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label htmFor="url">urls</label>
+          <input 
+          onChange={e => setUrl(e.target.value)}
+            value={url}>
+          </input>
+          <button></button>
+        </form>
+        <div>{url}</div>
       </header>
     </div>
   );
