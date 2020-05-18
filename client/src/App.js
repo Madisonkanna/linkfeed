36782bf0acf18b30c4be23aa4c511f291910c880
Madison Kanna <madison@mediavine.com>
2020-05-17 22:35:17 -0700

    back to use state

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,59 @@
-import React, { useReducer, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function reducer(state, action) {
-	const { currentUrls, pastUrls } = state;
-	if (action.type === 'ADD') {
-		return {
-			currentUrls: [
-				...currentUrls,
-				{
-					id: Math.random(),
-					link: action.value
-				}
-			]
-		};
+
+const App = () => {
+	const urlState = {
+		urls: useState([])
 	}
-	return;
-}
-
-function App() {
-	const input = useRef();
-	const [urls, dispatch] = useReducer(reducer, {
-		currentUrls: [],
-		pastUrls: []
-	});
-
-	const addUrl = e => {
-		e.preventDefault();
-		console.log(input.current.value);
-		dispatch({
-			type: 'ADD',
-			value: input.current.value
-		});
-		input.current.value = '';
-	};
-
+	const _refs = {
+		newUrl: undefined
+	}
 	return (
 		<div className="App">
-			<header className="App-header">
-				<form onSubmit={addUrl}>
-					<label htmFor="url">urls</label>
-					<input ref={input} />
-					<button type="submit" type="button" />
-				</form>
+			<div className="addUrl">
+				<form onSubmit={e => {
+					// by default submitted forms reload a page 
+					// for some reason.. prevent that: 
+					e.preventDefault()
+					// building the new array and setting it to 
+					// the state:
+          console.log(urlState.urls[1], '0 and 1')
+					urlState.urls[1]([
+						// destructure old urls into new array
+						...urlState.urls[0],
+						// add new url value to array
+						_refs.newUrl.value
+					])
 
-				<div>
-					Urls:
-          <ul>
-				  {urls.currentUrls.map(url => <li key={url.id}>{url.link}</li>)}
-          </ul>
-	
-				</div>
-			</header>
+					// clear the input value.. i think this is 
+					// how you can do it lol: 
+					_refs.newUrl.value = ''
+				}}>
+					<div>
+						<label>
+							New url: 
+							<input
+								ref={r => { _refs.newUrl = r }}
+							/>
+						</label>
+					</div>
+					<div>
+						<button type="submit">Add!</button>
+					</div>
+				</form>
+			</div>
+			<div className="urls">{
+				urlState.urls[0].map(url => <div>{url}</div>)
+			}</div>
 		</div>
-	);
+	)
 }
 
+
+
+
 export default App;
+
+
