cab29dc740f4f21dbf8f95e22da10a514acadc2f
Madison Kanna <madison@mediavine.com>
2020-05-17 21:09:37 -0700

    useReducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,59 @@
-import React, { useState } from 'react';
+import React, { useReducer, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function reducer(state, action) {
+	const { currentUrls, pastUrls } = state;
+	if (action.type === 'ADD') {
+		return {
+			currentUrls: [
+				...currentUrls,
+				{
+					id: Math.random(),
+					link: action.value
+				}
+			]
+		};
+	}
+	return;
+}
+
 function App() {
+	const input = useRef();
+	const [urls, dispatch] = useReducer(reducer, {
+		currentUrls: [],
+		pastUrls: []
+	});
 
-  const [url, setUrl] = useState('')
+	const addUrl = e => {
+		e.preventDefault();
+		console.log(input.current.value);
+		dispatch({
+			type: 'ADD',
+			value: input.current.value
+		});
+		input.current.value = '';
+	};
 
-  const onChange = e => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<form onSubmit={addUrl}>
+					<label htmFor="url">urls</label>
+					<input ref={input} />
+					<button type="submit" type="button" />
+				</form>
 
-    setUrl(e.target.value)
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Enter a link.
-        </p>
-        <form>
-          <label htmFor="url">urls</label>
-          <input 
-          onChange={onChange}
-            value={url}>
-          </input>
-          <button type="button"></button>
-        </form>
-        <div>{url}</div>
-      </header>
-    </div>
-  );
+				<div>
+					Urls:
+          <ul>
+				  {urls.currentUrls.map(url => <li key={url.id}>{url.link}</li>)}
+          </ul>
+	
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
